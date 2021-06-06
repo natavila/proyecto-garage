f63ddf6a0f6f73c756ad23e003c100caeedbdad4
SofiaSoengas <sofiasoengas1@gmail.com>
2021-06-06 19:23:55 -0300

    relacion cliente-plan

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Cliente.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Cliente.java
@@ -1,22 +1,18 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 @Entity
-public class Cliente{
-	
+public class Cliente {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nombre;
 	private String apellido;
 	private Integer dni;
@@ -24,70 +20,97 @@ public class Cliente{
 	private String email;
 	private String password;
 	private String roll;
-	
+
 	@ManyToOne
 	Usuario usuario;
-	
+
+	@ManyToOne
+	private Plan plan;
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
+
 	public Integer getDni() {
 		return dni;
 	}
+
 	public void setDni(Integer dni) {
 		this.dni = dni;
 	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	
+
 	public String getRoll() {
 		return roll;
 	}
+
 	public void setRoll(String roll) {
 		this.roll = roll;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Plan getPlan() {
+		return plan;
+	}
+
+	public void setPlan(Plan plan) {
+		this.plan = plan;
+	}
+
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", sexo=" + sexo
 				+ ", email=" + email + ", password=" + password + "]";
 	}
-	
-	
 
-	
-	
 }
