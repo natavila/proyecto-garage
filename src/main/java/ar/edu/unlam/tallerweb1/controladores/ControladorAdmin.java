45e70345584060b27a2e6fc975c98f679652efd2
SofiaSoengas <sofiasoengas1@gmail.com>
2021-06-06 21:36:31 -0300

    formulario crear plan y ver lista de planes

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorAdmin.java
@@ -3,10 +3,15 @@ package ar.edu.unlam.tallerweb1.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Auto;
+import ar.edu.unlam.tallerweb1.modelo.Cliente;
+import ar.edu.unlam.tallerweb1.modelo.Plan;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPlan;
 
 @Controller
@@ -27,4 +32,35 @@ public class ControladorAdmin {
 		return new ModelAndView("planes", modelo);
 	}
 
+	@RequestMapping("/crearPlan")
+	public ModelAndView registroPlan() {
+		ModelMap modelo = new ModelMap();
+		Plan plan = new Plan();
+		modelo.put("plan", plan);
+		return new ModelAndView("crearPlan", modelo);
+
+	}
+
+	@RequestMapping(path = "/crearPlan", method = RequestMethod.POST)
+	public ModelAndView validacionPlan(@ModelAttribute("plan") Plan plan) {
+
+		ModelMap modelo = new ModelMap();
+		servicioPlan.consultarPlan(plan.getId());
+
+		try {
+			if (plan.getNombre() != "" && plan.getCantidadAutosPermitidos() > 0
+					&& plan.getCantidadHorasPermitidas() > 0) {
+				servicioPlan.crearPlan(plan);
+				modelo.put("mensajeOk", "Plan creado correctamente");
+
+			} else {
+				modelo.put("mensajeError", "Error al crear el plan");
+			}
+		} catch (Exception e) {
+			modelo.put("mensajeError", "Error al crear el plan");
+			System.out.println(e.getMessage());
+		}
+
+		return new ModelAndView("crearPlan", modelo);
+	}
 }
